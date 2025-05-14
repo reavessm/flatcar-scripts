702bea437ddfe5e5126c781576afce6cdba55008
Krzesimir Nowak <knowak@microsoft.com>
2025-05-14 10:45:06 +0200

    fixup! build_toolchains: Break dep loop

--- build_library/catalyst_toolchains.sh
+++ build_library/catalyst_toolchains.sh
@@ -48,6 +48,7 @@ build_target_toolchain() {
     }
     # Breaking the following loops here:
     #
+    # cryptsetup[udev] -> libudev -> systemd -> cryptsetup
     # util-linux[audit] -> audit[python] -> python -> util-linux
     # util-linux[cryptsetup] -> cryptsetup -> util-linux
     # util-linux[selinux] -> libselinux[python] -> python -> util-linux
@@ -58,7 +59,8 @@ build_target_toolchain() {
     BDL_EQUERY=btt_bdl_equery \
     BDL_EMERGE=btt_bdl_emerge \
         break_dep_loop "${args_for_bdl[@]}" \
-            sys-apps/util-linux audit,cryptsetup,selinux,systemd,udev
+            sys-apps/util-linux audit,cryptsetup,selinux,systemd,udev \
+            sys-fs/cryptsetup udev
     unset btt_bdl_portageq btt_bdl_equery btt_bdl_emerge
 
     # --root is required because run_merge overrides ROOT=
